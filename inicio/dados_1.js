ed9bde2a8ec3a7b54889e287de8b4c40335f0f51
Daniel Mariano <60660967+Marianodan@users.noreply.github.com>
2022-03-29 22:45:12 -0300

    Trabalhando com Dados #01

---
+++ inicio/dados_1.js
@@ -0,0 +1,16 @@
+// Definindo quais são as variaveis
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+//definindo o que imprimir
+console.log("Caneta");
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+//Alterei o nome da Variavel
+nome = "Caneta BIC";
+console.log(nome);
