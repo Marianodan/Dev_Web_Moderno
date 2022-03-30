c926b7e38e41bbba94129a03b79d326bce310fa4
Daniel Mariano <60660967+Marianodan@users.noreply.github.com>
2022-03-29 21:57:14 -0300

    Commit 1

---
+++ inicio/desafio_sentenca_blocos.js
@@ -0,0 +1,18 @@
+// Desafio de aprimoramento de aprendizado - Exibir números ímpares e pares
+{
+    console.log("Bloco 1 - Números Ímpares");
+    console.log("Número 01");
+    console.log("Número 03");
+    console.log("Número 05");
+    console.log("Número 07");
+    console.log("Número 09");
+}
+
+{
+    console.log("Bloco 2 - Números Pares");
+    console.log("Número 02");
+    console.log("Número 04");
+    console.log("Número 06");
+    console.log("Número 08");
+    console.log("Número 10");
+}
