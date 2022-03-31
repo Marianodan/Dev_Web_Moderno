8a00a91f778741284a22f88665d61e0b6b1ff4be
Daniel Mariano <60660967+Marianodan@users.noreply.github.com>
2022-03-30 21:45:52 -0300

    Desafio de troca

---
+++ inicio/desafio_troca.js
@@ -0,0 +1,13 @@
+let a = 7;
+let b = 94;
+
+console.log("O valor de a = " + a);
+console.log("O valor de b = " + b);
+
+// A troca
+let temp = a;
+a = b;
+b = temp;
+
+console.log("O novo valor de a = " + a);
+console.log("O novo valor de b = " + b);
