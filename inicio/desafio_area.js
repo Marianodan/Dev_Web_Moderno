273cf4b50e550f0cd0da0dc654c1966aacdbe7c7
Daniel Mariano <60660967+Marianodan@users.noreply.github.com>
2022-03-30 21:13:00 -0300

    Desafio Área da Circunferência

---
+++ inicio/desafio_area.js
@@ -0,0 +1,17 @@
+// PI * raio * raio
+
+const PI = 3.14;
+let raio = 10;
+let areaCirc = PI * raio * raio
+
+console.log("O valor da área é " 
++ areaCirc 
++ "M2");
+
+// Outra forma de fazer é usando o:
+
+
+Math.PI 
+let Raio = 10
+
+console.log(Math.PI * Raio * Raio)
